af7a44d8e0783c933bd94ec021923dbef85d61f0
Ol1mowski <olimarki123@wp.pl>
2025-05-30 20:22:20 +0200

    files: add spring boot starting maven project

---
+++ server/src/main/java/pl/justdrinkjava/JustDrinkJava/JustDrinkJavaApplication.java
@@ -0,0 +1,13 @@
+package pl.justdrinkjava.JustDrinkJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDrinkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDrinkJavaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/pl/justdrinkjava/JustDrinkJava/JustDrinkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.justdrinkjava.JustDrinkJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDrinkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
