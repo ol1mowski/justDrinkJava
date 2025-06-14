6f3291c38c2640fe861a5e68a2ed917c822013b8
Oliwier <oliwier.markiewicz.dev@gmail.com>
2025-06-14 11:46:32 +0200

    Fix CVGuide imports and hook usage

--- client/src/components/CVGuide/CVGuide.tsx
+++ client/src/components/CVGuide/CVGuide.tsx
@@ -3,21 +3,21 @@ import { CVHeroSection } from './components/CVHeroSection.component'
 import { CVContent } from './components/CVContent.component'
 import { Tooltip } from './components/Tooltip.component'
 import { useTooltip } from './hooks/useTooltip.hook'
-import { tooltipData } from './types/tooltipData'
+import { tooltips } from './types/tooltipData'
 
 export const CVGuide = memo(() => {
-  const { activeTooltip, openTooltip, closeTooltip } = useTooltip()
+  const { activeTooltip, handleSectionClick, closeTooltip } = useTooltip()
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-java-orange/5 via-white to-java-orange/10">
       <CVHeroSection />
       <CVContent 
         activeTooltip={activeTooltip}
-        onSectionClick={openTooltip}
+        onSectionClick={handleSectionClick}
       />
       {activeTooltip && (
         <Tooltip 
-          data={tooltipData[activeTooltip]} 
+          data={tooltips[activeTooltip]} 
           onClose={closeTooltip} 
         />
       )}
