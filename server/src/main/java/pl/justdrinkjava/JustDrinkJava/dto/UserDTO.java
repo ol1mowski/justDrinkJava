d450421d59a35fcadc547c67d21174a4b330e0a4
Ol1mowski <olimarki123@wp.pl>
2025-06-23 10:49:33 +0200

    feat: add test coverage and husky to project

--- server/src/main/java/pl/justdrinkjava/JustDrinkJava/dto/UserDTO.java
+++ server/src/main/java/pl/justdrinkjava/JustDrinkJava/dto/UserDTO.java
@@ -1,1 +1,20 @@
- 
+package pl.justdrinkjava.JustDrinkJava.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    
+    private Long id;
+    private String email;
+    private String username;
+    private LocalDateTime createdAt;
+} 
